8c2759da5e4ee30c0d698155cb48f2850e7fb2dd
Andrei Negriu <81017870+NegriuAndrei@users.noreply.github.com>
2023-04-04 13:10:02 +0300

    User name changed

--- MobileProject/MainPage.xaml.cs
+++ MobileProject/MainPage.xaml.cs
@@ -13,7 +13,7 @@ public partial class MainPage : ContentPage
     private async void Login_Clicked(object sender, EventArgs e)
     {
             Credentials cred = new Credentials(entry_username.Text, entry_password.Text);
-            if (cred.GetUsername() == "andreimihai19" && cred.GetPassword() == "123456")
+            if (cred.GetUsername() == "admin" && cred.GetPassword() == "admin")
             {
                 await Navigation.PushAsync(new MenuPage());
             }
